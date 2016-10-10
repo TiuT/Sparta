7034fb800d7cd2487bff6774aa2b58d3a862a070
ginvar <shijianhua@192.168.202.128>
2016-10-10 23:52:29 +0800

    no message

--- Sparta/Pods/Target Support Files/Pods-Sparta/Pods-Sparta-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Sparta : NSObject
-@end
-@implementation PodsDummy_Pods_Sparta
-@end
--- Sparta/Sparta/Model/DAO/DB/OptDao/OptDao.h
+++
@@ -1,7 +0,0 @@
-#import "BaseDBDao.h"
-
-@interface OptDao : BaseDBDao
--(NSString*) getUpdateAppVersion;
--(NSArray*) queryOperatedList;
--(void) updateVersion:(NSString*) appVersion AndOptKeys:(NSArray*) optKeys;
-@end
